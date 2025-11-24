b950a76e51dacf39fe04d4195e3cf554ff69c4b7
Rovel-Coder <romain.veldeman@gmail.com>
2025-11-25 00:51:39 +0100

    .

--- src/components/GanttChart.vue
+++ src/components/GanttChart.vue
@@ -6,7 +6,7 @@ import GanttSidebar from './GanttSidebar.vue';
 import GanttToolbar from './GanttToolbar.vue';
 
 // Version du widget
-const WIDGET_VERSION = 'V0.0.7';
+const WIDGET_VERSION = 'V0.0.8';
 
 const props = defineProps<{ tasks: Task[] }>();
 

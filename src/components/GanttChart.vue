61cc4c91e88739f91244af57509f6a55a968190d
Rovel-Coder <romain.veldeman@gmail.com>
2025-11-27 08:20:06 +0100

    .

--- src/components/GanttChart.vue
+++ src/components/GanttChart.vue
@@ -397,7 +397,7 @@ function onBodyScroll(e: Event) {
 .gantt-top-left {
   grid-column: 1;
   grid-row: 1;
-  height: 103.5px;
+  height: 103px;
   background-color: #020617;
   border-right: 1px solid #374151;
   border-bottom: 1px solid #374151;
